e6e8eaa159985f6438f6e9b469386a9f95b292e3
Zainab Mohamed <zamohame@c2r12p2.42nice.fr>
2025-06-21 20:35:45 +0200

    initialisation of philos and forks, setup philos data

--- philo/init.c
+++ philo/init.c
@@ -1,21 +1,47 @@
 
-# include "philo.h"
+#include "philo.h"
 
-void    init_simulation(t_data *data, t_philo **philos, pthread_mutex_t **forks)
+t_philo	*init_philos(t_data *data)
 {
-    int i;
+	int		i;
+	t_philo	*philos;
 
-    *philos = malloc(sizeof(t_philo) * data->num_philos);
-    if (!*philos)
-        error_msg("Error: philo malloc failed :(", 1);
-    *forks = malloc(sizeof(pthread_mutex_t) * data->num_philos);
-    if (!*forks)
-        error_msg("Error: fork malloc failed :(", 1);
-    i = 0;
-    while (i < data->num_philos)
+	philos = malloc(sizeof(t_philo) * data->num_philos);
+	if (!philos)
+		error_msg("Error: philo malloc failed :(", 1);
+	return (philos);
+}
+
+pthread_mutex_t	*init_forks(t_data *data)
+{
+	int				i;
+	pthread_mutex_t	*forks;
+
+	forks = malloc(sizeof(pthread_mutex_t) * data->num_philos);
+	if (!forks)
+		error_msg("Error: forks malloc failed :(", 1);
+	i = 0;
+	while (i < data->num_philos)
 	{
-		if (pthread_mutex_init(&(*forks)[i], NULL) != 0)
-			error_msg("fork mutex init failed", 1);
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+	return (forks);
+}
+
+void	setup_philos(t_data *data, t_philo *philos, pthread_mutex_t *forks)
+{
+	int i;
+
+	i = 0;
+	while (i < data->num_philos)
+	{
+		philos[i].id = i + 1;
+		philos[i].data = data;
+		philos[i].meals_eaten = 0;
+		philos[i].last_meal_time = 0;
+		philos[i].left_fork = &forks[i];
+		philos[i].right_fork = &forks[(i + 1) % data->num_philos];
 		i++;
 	}
 }
--- philo/philo.h
+++ philo/philo.h
@@ -2,37 +2,41 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <limits.h>
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
-typedef struct s_data 
+typedef struct s_data
 {
-    int		num_philos;
-    int		time_to_die;
-    int		time_to_eat;
-    int		time_to_sleep;
-    int		num_times;
-}	t_data;
+	int				num_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_times;
+}					t_data;
 
 typedef struct s_philo
 {
 	int				id;
 	int				meals_eaten;
-	long long		last_meal_time; 
-	pthread_t		thread_id;    
-	pthread_mutex_t	*left_fork;  
-	pthread_mutex_t	*right_fork;    
-	struct s_data	*data;        
-}	t_philo;
+	long long		last_meal_time;
+	pthread_t		thread_id;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	struct s_data	*data;
+}					t_philo;
 
+long				long_atoi(const char *str);
+int					is_valid_arg(char *arg);
+void				error_msg(char *msg, int exit_code);
+void				init_data(t_data *data);
+void				parse_args(t_data *data, int argc, char **argv);
 
-long        long_atoi(const char *str);
-int         is_valid_arg(char *arg);
-void        error_msg(char *msg, int exit_code);
-void	    init_data(t_data *data);
-void	    parse_args(t_data *data, int argc, char **argv);
+t_philo				*init_philos(t_data *data);
+pthread_mutex_t		*init_forks(t_data *data);
+void				setup_philos(t_data *data, t_philo *philos,
+						pthread_mutex_t *forks);
 
 #endif
